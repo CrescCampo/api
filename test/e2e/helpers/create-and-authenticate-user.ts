40f826a59458cd33103575a24c4c28b4407428c1
matheusandrade <matheusandrade.ma2003@gmail.com>
2026-05-02 11:35:31 -0300

    feat: add a secure password

--- test/e2e/helpers/create-and-authenticate-user.ts
+++ test/e2e/helpers/create-and-authenticate-user.ts
@@ -7,14 +7,14 @@ export default async function createAndAuthenticateUser(
   await request(app.getHttpServer()).post('/auth/register').send({
     name: 'Usuário Teste',
     email: 'user@teste.com',
-    password: 'senha-123',
+    password: 'senha-muito-segura123',
   });
 
   const loginResponse = await request(app.getHttpServer())
     .post('/auth/login')
     .send({
       email: 'user@teste.com',
-      password: 'senha-123',
+      password: 'senha-muito-segura123',
     });
 
   return loginResponse.body.token;
