022512f95cbbe053d14075d30d844c54ab23582f
matheusandrade <matheusandrade.ma2003@gmail.com>
2026-05-02 11:35:43 -0300

    feat: override guards for tests

--- test/e2e/helpers/test-app-factory.ts
+++ test/e2e/helpers/test-app-factory.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ThrottlerGuard } from '@nestjs/throttler';
 import AppModule from 'infra/app.module';
+import EmailIpThrottlerGuard from 'infra/auth/email-ip-throttler.guard';
 
 export default class TestAppFactory {
   static async create(): Promise<INestApplication> {
+    const allowAll = { canActivate: () => true };
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(ThrottlerGuard)
+      .useValue(allowAll)
+      .overrideGuard(EmailIpThrottlerGuard)
+      .useValue(allowAll)
+      .compile();
 
     const app = moduleFixture.createNestApplication();
     app.useLogger(false);
